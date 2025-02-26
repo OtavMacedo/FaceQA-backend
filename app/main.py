e47f7d644467890ba600abf534d154e998cc350f
otavmacedo <otavmacedo04@gmail.com>
2025-02-26 19:31:03 -0300

    feat: Add API router for version 1 and update main application to include it

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-from app.api.routes.router import router as api_router
+from app.api.v1.routers import router
 from app.core.app_settings import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f'{settings.API_V1_STR}/openapi.json',
 )
-app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(router, prefix=settings.API_V1_STR)
 
 
 @app.get('/')
