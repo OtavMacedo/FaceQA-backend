f3cf6fd3b94347542f3e741594816383265ea4c6
otavmacedo <otavmacedo04@gmail.com>
2025-02-27 22:11:49 -0300

    feat: add user repository

---
+++ app/repositories/user.py
@@ -0,0 +1,29 @@
+from fastapi import Depends
+from sqlalchemy import Select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.session import get_session
+from app.models.user import User
+from app.schemas.user import UserSchema
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession = Depends(get_session)):
+        self.session = session
+
+    async def create(self, user: UserSchema):
+        db_user = User(
+            email=user.email,
+            hashed_password=user.password,  # get_password_hash
+        )
+        self.session.add(db_user)
+        await self.session.commit()
+        await self.session.refresh(db_user)
+
+        return db_user
+
+    async def read_by_email(self, email: str):
+        db_user = await self.session.scalar(
+            Select(User).where(User.email == email)
+        )
+        return db_user
