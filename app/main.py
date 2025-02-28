cef1a9ca7ca175e1960647a3ca0ba53ffe3e86f6
otavmacedo <otavmacedo04@gmail.com>
2025-02-27 23:02:37 -0300

    fix: rename router import for clarity and update openapi_url path

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-from app.api.v1.routers import router
+from app.api.v1.routers import router as v1_router
 from app.core.app_settings import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f'{settings.API_V1_STR}/openapi.json',
+    openapi_url='/openapi.json',
 )
-app.include_router(router, prefix=settings.API_V1_STR)
+app.include_router(v1_router, prefix=settings.API_V1_STR)
 
 
 @app.get('/')
