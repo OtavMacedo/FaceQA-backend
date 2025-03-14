c7a27dfde811bfb246d3a4572306f53622de12da
otavio <otavmacedo04@gmail.com>
2025-03-13 21:47:43 -0300

    feat: add API key creation endpoint and update router to include it

---
+++ app/api/v1/endpoints/api_keys.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends
+
+from app.core.security import get_current_user
+from app.models.user import User
+from app.repositories.api_key import APIKeyRepository
+from app.schemas.api_key import APIKeySchema
+from app.services.api_key_service import generate_api_key
+
+router = APIRouter(prefix='/api-keys', tags=['APIKey'])
+
+
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=APIKeySchema)
+async def create_api_key(
+    api_key_repository: APIKeyRepository = Depends(APIKeyRepository),
+    current_user: User = Depends(get_current_user)
+):
+    api_key = generate_api_key()
+
+    db_api_key = await api_key_repository.create(
+        api_key=api_key, user=current_user
+    )
+    return APIKeySchema(api_key=api_key, id=db_api_key.id)
--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import api_keys, auth, users
 
 router = APIRouter()
 
-router.include_router(users.router)
+router.include_router(api_keys.router)
 router.include_router(auth.router)
+router.include_router(users.router)
